b824f067471ea002aaceff936e4ca9088b1000b5
CambridgeinstituteoftechnologyBLR-Likitha <likitha.24ece@cambridge.edu.in>
2026-05-29 15:42:29 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -7,7 +7,62 @@ module project (
     output [7:0] uio_oe,
 
     input  ena,
+    input  clk,```verilog id="7vq2ma"
+module tt_um_likitha_trng (
+
+    input  [7:0] ui_in,
+    output [7:0] uo_out,
+
+    input  [7:0] uio_in,
+    output [7:0] uio_out,
+    output [7:0] uio_oe,
+
+    input  ena,
     input  clk,
+    input  rst_n
+);
+
+wire entropy_feedback_a;
+wire entropy_feedback_b;
+
+assign entropy_feedback_a =
+        ui_in[7] ^
+        ui_in[5];
+
+assign entropy_feedback_b =
+        ui_in[3] ^
+        ui_in[1];
+
+assign uo_out[0] =
+        ui_in[0] ^ entropy_feedback_a;
+
+assign uo_out[1] =
+        ui_in[1] ^ entropy_feedback_b;
+
+assign uo_out[2] =
+        ui_in[2] ^ entropy_feedback_a;
+
+assign uo_out[3] =
+        ui_in[3] ^ entropy_feedback_b;
+
+assign uo_out[4] =
+        ui_in[4] ^ entropy_feedback_a;
+
+assign uo_out[5] =
+        ui_in[5] ^ entropy_feedback_b;
+
+assign uo_out[6] =
+        ui_in[6] ^ entropy_feedback_a;
+
+assign uo_out[7] =
+        ui_in[7] ^ entropy_feedback_b;
+
+assign uio_out = 8'b00000000;
+assign uio_oe  = 8'b00000000;
+
+endmodule
+```
+
     input  rst_n
 );
 
