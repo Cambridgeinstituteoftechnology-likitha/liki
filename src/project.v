a46e4615b00f2dcb9779c1eeb87cf95bc48437d2
CambridgeinstituteoftechnologyBLR-Likitha <likitha.24ece@cambridge.edu.in>
2026-05-29 15:37:39 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,52 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
+module project (
+    input  [7:0] ui_in,
+    output [7:0] uo_out,
 
-`default_nettype none
+    input  [7:0] uio_in,
+    output [7:0] uio_out,
+    output [7:0] uio_oe,
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  ena,
+    input  clk,
+    input  rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+wire entropy_feedback_a;
+wire entropy_feedback_b;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+assign entropy_feedback_a =
+        ui_in[7] ^
+        ui_in[5];
+
+assign entropy_feedback_b =
+        ui_in[3] ^
+        ui_in[1];
+
+assign uo_out[0] =
+        ui_in[0] ^ entropy_feedback_a;
+
+assign uo_out[1] =
+        ui_in[1] ^ entropy_feedback_b;
+
+assign uo_out[2] =
+        ui_in[2] ^ entropy_feedback_a;
+
+assign uo_out[3] =
+        ui_in[3] ^ entropy_feedback_b;
+
+assign uo_out[4] =
+        ui_in[4] ^ entropy_feedback_a;
+
+assign uo_out[5] =
+        ui_in[5] ^ entropy_feedback_b;
+
+assign uo_out[6] =
+        ui_in[6] ^ entropy_feedback_a;
+
+assign uo_out[7] =
+        ui_in[7] ^ entropy_feedback_b;
+
+assign uio_out = 8'b00000000;
+assign uio_oe  = 8'b00000000;
 
 endmodule
